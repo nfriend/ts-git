9a18ce120b6c27774410c6a24534ff606a2255cf
Nathan Friend <nathan@gitlab.com>
2019-04-01 13:12:49 -0300

    Add UnderConstruction component

---
+++ src/components/UnderConstruction.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="jumbotron mt-5">
+    <h1 class="display-4">Hello, world!</h1>
+    <p class="lead">
+      Welcome to <code>ts-git</code>. This project is currently under
+      construction.
+    </p>
+    <hr class="my-4" />
+    <p>
+      In the meantime, check out the source on
+      <a href="https://gitlab.com/nfriend/ts-git">GitLab</a>.
+    </p>
+    <a
+      class="btn btn-primary btn-lg"
+      href="https://gitlab.com/nfriend/ts-git"
+      role="button"
+      >View source</a
+    >
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class UnderConstruction extends Vue {}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <UnderConstruction />
   </div>
 </template>
 
-<script lang="ts">
+<script>
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import UnderConstruction from '@/components/UnderConstruction.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    UnderConstruction,
   },
 })
 export default class Home extends Vue {}
