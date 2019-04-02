097b58ac9211e0f1dc200749200648903e50aaa8
Nathan Friend <nathan@gitlab.com>
2019-04-02 08:22:22 -0300

    Publish CLI

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
+import { MyTestClass } from '@nathanfriend/ts-git';
 import * as yargs from 'yargs';
 
 console.log('something: ' + yargs.argv.something);
+const test = new MyTestClass();
+test.doSomething();
