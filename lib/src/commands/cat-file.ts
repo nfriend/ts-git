594fad9a263666ff06e8e3d0836d752b490ec5fc
Nathan Friend <nathan@gitlab.com>
2019-04-06 11:43:03 -0300

    Untested cat-file

--- lib/src/commands/cat-file.ts
+++ lib/src/commands/cat-file.ts
@@ -1,5 +1,10 @@
 import { CommandResult } from './CommandResult';
 import { findRepoRoot } from '../../util/file-system/find-repo-root';
+import * as path from 'path';
+import * as bluebird from 'bluebird';
+
+const zlib = require('zlib');
+bluebird.promisifyAll(zlib);
 
 export type GitObjectType = 'blob' | 'commit' | 'tag' | 'tree';
 
@@ -17,6 +22,38 @@ export const catFileCommand = async (
         'fatal: not a git repository (or any of the parent directories): .git',
     };
   }
+
+  const objDirectory = object.substring(0, 2);
+  const objFilename = object.substring(2);
+
+  let fileBuffer: Buffer;
+  try {
+    const fileBuffer = await fs.readFileAsync(
+      path.resolve(repoRoot, '.git', objDirectory, objFilename),
+    );
+  } catch (err) {
+    return {
+      success: false,
+      message: `fatal: Not a valid object name ${object}`,
+    };
+  }
+
+  let fileContents: string;
+  try {
+    fileContents = await zlib.inflateAsync(fileBuffer);
+  } catch (err) {
+    return {
+      success: false,
+
+      // TODO: what message does the real git show in this situation?
+      message: `fatal: Unable to decompress object ${object}`,
+    };
+  }
+
+  return {
+    success: true,
+    message: fileContents,
+  };
 
   throw new Error('cat-file is not yet implemented!');
 };
