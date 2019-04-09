e0e5a3b27c1863a95f7fa593c724521ff2eed3d3
Nathan Friend <nathan@gitlab.com>
2019-04-09 18:02:54 -0300

    Basic terminal implemented

---
+++ demo-app/src/components/Terminal.vue
@@ -0,0 +1,35 @@
+<template>
+  <VueTerminal
+    :task-list="taskList"
+    :command-list="commandList"
+    :title="'test'"
+  />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import VueTerminal from 'vue-terminal';
+
+@Component({
+  components: {
+    VueTerminal,
+  },
+})
+export default class Terminal extends Vue {
+  taskList = {
+    defaultTask: {
+      description: 'this is default task.',
+      async defaultTask(pushToList) {
+        return await {
+          type: 'success',
+          label: 'Success',
+          message: 'Hello, world!',
+        };
+      },
+    },
+  };
+  commandList = {
+    // your commands
+  };
+}
+</script>
---
+++ demo-app/src/views/Test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <Terminal />
+  </div>
+</template>
+
+<script>
+import { Component, Vue } from 'vue-property-decorator';
+import Terminal from '@/components/Terminal.vue';
+
+@Component({
+  components: {
+    Terminal,
+  },
+})
+export default class Test extends Vue {}
+</script>
