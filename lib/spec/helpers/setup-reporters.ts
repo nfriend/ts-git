55ee3287afd24a4ae17139184444f1225a1e54d5
Nathan Friend <nathan@gitlab.com>
2019-10-01 19:45:06 -0300

    Migrate from Karma to Jest, rework CI

--- lib/spec/helpers/setup-reporters.ts
+++
@@ -1,8 +0,0 @@
-import * as jasmineReporters from 'jasmine-reporters';
-
-const junitReporter = new jasmineReporters.JUnitXmlReporter({
-  savePath: 'test-results',
-  consolidateAll: true,
-});
-
-jasmine.getEnv().addReporter(junitReporter);
